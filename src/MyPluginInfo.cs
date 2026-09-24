8f6cdf24d803e023cc15ab3a8f6e0c06d968de1d
mleem97 <marvinleede@gmail.com>
2026-09-25 00:51:23 +0200

    chore: version bump + changelog

--- src/MyPluginInfo.cs
+++ src/MyPluginInfo.cs
@@ -3,6 +3,6 @@ namespace CableTracer
     internal static class MyPluginInfo
     {
         internal const string PLUGIN_NAME    = "gregMod.CableTracer";
-        internal const string PLUGIN_VERSION = "1.1.0";
+        internal const string PLUGIN_VERSION = "1.1.1";
     }
 }
